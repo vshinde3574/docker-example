2e546b945cd0e61f9f476a97be794c8e9b1ea571
VAIBHAV SHINDE <vaibhavshinde@VAIBHAVs-MacBook-Air.local>
2022-07-20 12:43:27 +0530

    committed

--- src/main/java/com/dockerexample/dockerexample/RawFunction.java
+++ src/main/java/com/dockerexample/dockerexample/RawFunction.java
@@ -3,7 +3,7 @@ package com.dockerexample.dockerexample;
 import com.google.cloud.functions.Context;
 import com.google.cloud.functions.RawBackgroundFunction;
 
-public class RawFUnction implements RawBackgroundFunction {
+public class RawFunction implements RawBackgroundFunction {
 
     @Override
     public void accept(String json, Context context) throws Exception {
