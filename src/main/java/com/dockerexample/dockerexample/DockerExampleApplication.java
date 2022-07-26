69157ae49b24d8f7d3b4390feb859fccd8d28879
VAIBHAV SHINDE <vaibhavshinde@VAIBHAVs-MacBook-Air.local>
2022-07-26 11:14:09 +0530

    cloud function

--- src/main/java/com/dockerexample/dockerexample/DockerExampleApplication.java
+++ src/main/java/com/dockerexample/dockerexample/DockerExampleApplication.java
@@ -3,11 +3,31 @@ package com.dockerexample.dockerexample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 @SpringBootApplication
 public class DockerExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DockerExampleApplication.class, args);
+
+		List<PubSub> list = new ArrayList<>();
+		list.add(new PubSub("xyz", "id4", "01"));
+		list.add(new PubSub("pqr", "id2", "01"));
+		list.add(new PubSub("abc", "id3", "01"));
+
+		list.sort(new Comparator<PubSub>() {
+			@Override
+			public int compare(PubSub o1, PubSub o2) {
+				return o1.getData().compareTo(o2.getData());
+			}
+		});
+		list.stream().forEach(o -> System.out.println(o.getData()));
+
+
+		list.stream().sorted((o1, o2) -> o1.getMessageId().compareTo(o2.getMessageId())).forEach(o->System.out.println(o.getMessageId()));
 	}
 
 }
--- src/main/java/com/dockerexample/dockerexample/PubSub.java
+++ src/main/java/com/dockerexample/dockerexample/PubSub.java
@@ -7,4 +7,42 @@ public class PubSub {
     Map<String, String> attributes;
     String messageId;
     String publishTime;
+
+    public PubSub(String data, String messageId, String publishTime) {
+        this.data = data;
+        this.messageId = messageId;
+        this.publishTime = publishTime;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public Map<String, String> getAttributes() {
+        return attributes;
+    }
+
+    public void setAttributes(Map<String, String> attributes) {
+        this.attributes = attributes;
+    }
+
+    public String getMessageId() {
+        return messageId;
+    }
+
+    public void setMessageId(String messageId) {
+        this.messageId = messageId;
+    }
+
+    public String getPublishTime() {
+        return publishTime;
+    }
+
+    public void setPublishTime(String publishTime) {
+        this.publishTime = publishTime;
+    }
 }
